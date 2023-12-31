41e6483405d137250437ee88493fea1e22917212
peace-Van <105348805+peace-Van@users.noreply.github.com>
2023-12-30 22:27:31 -0800

    New logarithmic mode for binary search of T

--- binary_search_T.py
+++ binary_search_T.py
@@ -5,26 +5,31 @@ Created on Sun Nov 26 00:02:05 2023
 @author: Tracy
 """
 
+import math
+
 
 # Crystal clustering based on binary search of `T` for a desired number of clusters `target_k`
-# Search range (0, 2 * model.theoT)
-def binary_search_T(model, target_k, verbose=False):
-    low = 0
-    high = model.theoT * 2
+# bounds is the searching range for `T`
+def binary_search_T(model, target_k, bounds=[0, 1], logarithmic=False, verbose=False):
+    low = math.log10(bounds[0]) if logarithmic else bounds[0]
+    high = math.log10(bounds[1]) if logarithmic else bounds[1]
+
     while True:
         curr = (low + high) / 2
-        model.reset(new_temp=curr)
+        new_temp = 10 ** curr if logarithmic else curr
+        model.reset(new_temp=new_temp)
         idx = model.fit_predict(verbose=False)
         n_comps = idx.max() + 1
         if verbose:
-            print('Temperature: %.1f, N_clusters: %d' % (curr, n_comps))
+            print('Temperature: %.4f, Num_clusters: %d' % (new_temp, n_comps))
         if n_comps == target_k:
             break
         elif n_comps < target_k:
             low = curr
         else:
             high = curr
-    return curr, idx
+
+    return new_temp, idx
 
 
 if __name__ == '__main__':
@@ -37,7 +42,14 @@ if __name__ == '__main__':
     k = 3
 
     # Initialize with data
-    cc = CrystalCluster(X, weights=None, temperature=None, k=None)
+    cc = CrystalCluster(X, standardize=True, weights=None,
+                        temperature=None, k=None)
     # The theoretical temperature
     print(cc.theoT)
-    T, idx = binary_search_T(cc, k, verbose=True)
+
+    print('logarithmic binary search')
+    T, idx = binary_search_T(
+        cc, k, bounds=[1e-4, 1], logarithmic=True, verbose=True)
+    print('linear binary search')
+    T, idx = binary_search_T(
+        cc, k, bounds=[0, 1], logarithmic=False, verbose=True)
